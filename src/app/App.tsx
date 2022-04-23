0c4f5dced9d810ddac8f0a4d260c325408a312aa
Rafsanjani Aziz <coded_raf@yahoo.com>
2022-04-23 20:13:28 +0100

    Scroll to top of page on navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import { Navigation } from "../navigation/Navigation";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { Landing } from "../components/landing/Landing";
 import { About } from "../pages/about/About";
 import { PodcastDetail } from "../pages/podcast-detail/PodcastDetail";
@@ -10,6 +15,7 @@ export function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Navigation />
         <Routes>
           <Route path="/" element={<Landing />} />
@@ -19,4 +25,14 @@ export function App() {
       </Router>
     </div>
   );
+}
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
